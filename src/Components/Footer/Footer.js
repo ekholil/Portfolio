067f0a3e442a466fcaedfaf52383d33334f145b6
Ibrahim Kholilullah <kholilebrahim01@gmail.com>
2021-12-26 19:36:05 +0600

    changed footer year

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div style={{padding:'20px 0', backgroundColor:'#212F3D', textAlign:'center', color: '#fff'}}>
-            <p>Ibrahim Kholilullah &copy; 2021. All Rights Reserved.</p>
+            <p>Ibrahim Kholilullah &copy; 2022. All Rights Reserved.</p>
         </div>
     );
 };
